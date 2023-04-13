d9f5d26435d60f7f2cf5fd8d926ad9be66ce094b
SHEA <nacho.dieguez18@gmail.com>
2023-04-13 20:53:57 -0300

    search page and categoryNav component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -23,7 +23,7 @@ const Header = () => {
   return (
     <header className='bg-primary py-6 fixed w-full top-0 z-40 lg:relative xl:mb-[30px]'>
       <div className='container mx-auto'>
-        <div className='flex flex-row gap-4 lg:items-center justify-between mb-4 lg:mb-0'>
+        <div className='flex flex-row gap-4 lg:items-center justify-between mb-4 xl:mb-0'>
           <div onClick={() => setCatNavMobile(true)} className='text-3xl xl:hidden cursor-pointer'>
             <FiMenu />
           </div>
@@ -31,23 +31,25 @@ const Header = () => {
             <CategoryNavMobile setCatNavMobile={setCatNavMobile}/>
           </div>
           <Link to={'/'}>
-            <img src={logo} alt='logo'/>
+            <img className='h-11' src={logo} alt='logo'/>
           </Link>
           <div className='hidden w-full xl:flex xl:max-w-[734px]'>
             <SearchForm />
           </div>
-          <div>
-            <div>Need help? 123 456 789</div>
+          <div className='flex items-center gap-x-[10px]'>
+            <div className='hidden xl:flex uppercase'>Need help? 123 456 789</div>
             <div onClick={() => setIsOpen(!isOpen)} className='relative cursor-pointer'>
               <SlBag className='text-2xl' />
-              <div>2</div>
+              <div className='bg-accent text-primary absolute w-[18px] h-[19px] rounded-full top-3 -right-1 text-[13px] flex justify-center items-center font-bold tracking-[-0.1em]'>
+                2
+              </div>
             </div>
             <div className={`${isOpen ? 'right-0' : '-right-full'} bg-[#0e0f10] shadow-xl fixed top-0 bottom-0 w-full z-10 md:max-w-[500px] transition-all duration-300`}>
               <Cart />
             </div>
           </div>
         </div>
-        <div className='lg:hidden'>
+        <div className='xl:hidden'>
           <SearchForm />
         </div>
       </div>
