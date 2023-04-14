9092d46f1cfef3f2526a516453aa648723ec30fa
SHEA <nacho.dieguez18@gmail.com>
2023-04-14 16:31:14 -0300

    ProductDetails page

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -19,6 +19,9 @@ const ProductDetails = () => {
     return <div className='container mx-auto'>Loading...</div>
   };
 
+  //category title
+  const categoryTitle = data[0].attributes.categories.data[0].attributes.title;
+
   return (
     <div className='mb-16 pt-44 lg:pt-[30px] xl:pt-0'>
       <div className='container mx-auto'>
@@ -38,7 +41,7 @@ const ProductDetails = () => {
             </div>
           </div>
         </div>
-        <RelatedProducts />
+        <RelatedProducts categoryTitle={categoryTitle}/>
       </div>
     </div>
   );
