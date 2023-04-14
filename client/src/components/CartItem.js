5923e72b1158bccd2f88cec2760b29e31a4eae11
SHEA <nacho.dieguez18@gmail.com>
2023-04-14 19:46:47 -0300

    Cart components

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { IoClose } from 'react-icons/io5'
+import { IoClose } from 'react-icons/io5';
 import Qty from './Qty';
+import { CartContext } from '../context/CartContext';
 
 
 const CartItem = ({ item }) => {
+
+  const { removeFromCart } = useContext(CartContext);
+
   return (
     <div className='flex gap-x-8'>
       <Link to={`product/${item.id}`} className='w-[70px] h-[70px]'>
@@ -13,15 +17,15 @@ const CartItem = ({ item }) => {
       <div className='flex-1'>
         <div className='flex gap-x-4 mb-3 '>
           <Link to={`product/${item.id}`}>{item.attributes.title}</Link>
-          <div className='cursor-pointer text-[24px] hover:text-accent transition-all'>
+          <div onClick={() => removeFromCart(item.id)} className='cursor-pointer text-[24px] hover:text-accent transition-all'>
             <IoClose />
           </div>
         </div>
         <div className='flex items-center gap-x-12'>
           <div className='flex gap-x-4 mb-2'>
             <Qty item={item} />
-            <div className='text-accent text-xl'>$ {item.attributes.price * item.amount}</div>
           </div>
+          <div className='text-accent text-xl'>$ {item.attributes.price * item.amount}</div>
         </div>
         <div>
           <span className='text-accent'>
--- client/src/context/CartContext.js
+++ client/src/context/CartContext.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
 
 //create context
 export const CartContext = createContext();
@@ -9,6 +9,23 @@ const CartProvider = ({ children }) => {
   const [itemsAmount, setItemsAmount] = useState(0);
   const [amount, setAmount] = useState(0);
   const [total, setTotal] = useState(0);
+
+  //cart amount
+  useEffect(() => {
+    const amount = cart.reduce((a, c) => {
+      return a + c.amount;
+    }, 0);
+
+    setItemsAmount(amount);
+  }, [cart]);
+
+  //cart total
+  useEffect(() => {
+    const total = cart.reduce((a, c) => {
+      return a + c.attributes.price * c.amount
+    }, 0);
+    setTotal(total);
+  }, [cart]);
 
   //add to cart
   const addToCart = (item, id) => {
@@ -37,8 +54,83 @@ const CartProvider = ({ children }) => {
     setIsOpen(true);
   };
 
+  //remove from cart
+  const removeFromCart = (id) => {
+    const newCart = cart.filter(item => {
+      return item.id !== id;
+    });
+    setCart(newCart);
+  };
+  
+  //handle input
+  const handleInput = (e, id) => {
+    const value = parseInt(e.target.value);
+    //find the item in the cart by id
+    const cartItem = cart.find(item => {
+      return item.id === id;
+    });
+    if(cartItem) {
+      const newCart = cart.map(item => {
+        if(item.id === id) {
+          if(isNaN(value)) {
+            setAmount(1);
+            return { ...item, amount:1};
+          } else {
+            setAmount(value);
+            return { ...item, amount: value};
+          }
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    }
+    setIsOpen(true);
+  };
+
+  //handle select
+  const handleSelect = (e, id) => {
+    const value =  parseInt(e.target.value);
+    const cartItem = cart.find(item => {
+      return item.id === id;
+    });
+    if(cartItem) {
+      const newCart = [...cart].map(item => {
+        if(item.id === id) {
+          setAmount(value);
+          return { ...item, amount: value};
+        } else {
+          return item;
+        }
+      });
+
+      setCart(newCart);
+    }
+  };
+
+  //clear cart
+  const clearCart = () => {
+    setCart([]);
+  };
+
+
   return (
-    <CartContext.Provider value={{ isOpen, setIsOpen, addToCart, cart }}>{children}</CartContext.Provider>
+    <CartContext.Provider
+      value={{
+        isOpen,
+        setIsOpen,
+        addToCart,
+        cart,
+        removeFromCart,
+        itemsAmount,
+        handleInput,
+        handleSelect,
+        total,
+        clearCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
   );
 };
 
