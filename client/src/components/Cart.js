afbe92879400c7a8b0fd6880d4bff4a4356739f8
SHEA <nacho.dieguez18@gmail.com>
2023-04-14 17:40:37 -0300

    CartItem component

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -3,16 +3,23 @@ import React, { useContext } from 'react';
 import { IoClose } from 'react-icons/io5';
 //context
 import { CartContext } from '../context/CartContext';
+//components
+import CartItem from './CartItem';
 
 const Cart = () => {
 
-  const { setIsOpen } = useContext(CartContext);
+  const { setIsOpen, cart } = useContext(CartContext);
 
   return (
     <div className='w-ful h-full px-4 text-white'>
       <div>
         <div onClick={() => setIsOpen(false)} className='text-4xl w-20 h-[98px] flex justify-start items-center cursor-pointer'>
           <IoClose />
+        </div>
+        <div>
+          {cart.map((item) => {
+            return <CartItem item={item} key={item.id}/>;
+          })}
         </div>
       </div>
     </div>
--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { IoClose } from 'react-icons/io5'
+import Qty from './Qty';
 
-const CartItem = () => {
-  return <div>CartItem</div>;
+
+const CartItem = ({ item }) => {
+  return (
+    <div className='flex gap-x-8'>
+      <Link to={`product/${item.id}`} className='w-[70px] h-[70px]'>
+        <img src={`http://localhost:1337${item.attributes.image.data.attributes.url}`} alt='product'/>
+      </Link>
+      <div className='flex-1'>
+        <div className='flex gap-x-4 mb-3 '>
+          <Link to={`product/${item.id}`}>{item.attributes.title}</Link>
+          <div className='cursor-pointer text-[24px] hover:text-accent transition-all'>
+            <IoClose />
+          </div>
+        </div>
+        <div className='flex items-center gap-x-12'>
+          <div className='flex gap-x-4 mb-2'>
+            <Qty item={item} />
+            <div className='text-accent text-xl'>$ {item.attributes.price * item.amount}</div>
+          </div>
+        </div>
+        <div>
+          <span className='text-accent'>
+            $ {item.attributes.price} per piece
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
