983a3b429a03a86a0257c152e8eddde5416f9075
SHEA <nacho.dieguez18@gmail.com>
2023-04-16 18:19:07 -0300

    ready to deployment

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -12,7 +12,7 @@ const CartItem = ({ item }) => {
   return (
     <div className='flex gap-x-8'>
       <Link to={`product/${item.id}`} className='w-[70px] h-[70px]'>
-        <img src={`http://localhost:1337${item.attributes.image.data.attributes.url}`} alt='product'/>
+        <img src={`https://cameramark.onrender.com${item.attributes.image.data.attributes.url}`} alt='product'/>
       </Link>
       <div className='flex-1'>
         <div className='flex gap-x-4 mb-3 '>
